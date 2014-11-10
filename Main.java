ce7d9dd70ccafbac2bc8f00925bb6a8d74f7bde8
Fridman <bgfridman@gmail.com>
2014-11-10 13:17:39 -0500

    main file

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    }
+		connectDB("jdbc:postgresql://localhost:5432/csc343h-c4fridma", "c4fridma", "password");
+		disconnectDB();
+	}
 }
