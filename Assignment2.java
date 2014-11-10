2d412e5e99296dfdadad0d3953e568ae0c5617c8
paulkirow <paul.kirow@mail.utoronto.ca>
2014-11-10 14:24:04 -0500

    Fixed all  the bugs

--- Assignment2.java
+++ Assignment2.java
@@ -1,99 +1,105 @@
+
 import java.sql.*;
 
 public class Assignment2 {
-    
-  // A connection to the database  
-  Connection connection;
-  
-  // Statement to run queries
-  Statement sql;
-  
-  // Prepared Statement
-  PreparedStatement ps;
-  
-  // Resultset for the query
-  ResultSet rs;
-  
-  //CONSTRUCTOR
-  Assignment2(){
-  try {	//Load JDBC driver
-	Class.forName("org.postgresql.Driver");
-  } catch (ClassNotFoundException e) {
-	return;
-  }
-  }
-  
-  //Using the input parameters, establish a connection to be used for this session. Returns true if connection is sucessful
-  public boolean connectDB(String URL, String username, String password){
-    try{ 
-	connection = DriverManager.getConnection(URL,username,password);
-	if (connection == null) {return false;}
-	sql = connection.createStatement();
-	} catch (SQLException e) {return false;}
-	return true;
-  }
-  
-  //Closes the connection. Returns true if closure was sucessful
-  public boolean disconnectDB(){
-	try {
-		connection.close();  
-		return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-  }
-  
-  public boolean insertCountry (int cid, String name, int height, int population) {
-	String stmt = "INSERT INTO country " +
-                   "VALUES (?, ?, ?, ?)";
-	PreparedStatement preStmt = connection.preparedStatement(stmt);
-	preStmt.setString(1,cid);
-	preStmt.setString(2,name);
-	preStmt.setString(3,height);
-	preStmt.setString(4,population);
-	try {preStmt.executeUpdate(sql);}
-	catch (SQLException e) {return false;}
-	return true;
-  }
 
-  public int int getCountriesNextToOceanCount(int oid) {
-	String stmt = "SELECT COUNT(cid) AS num FROM oceanAccess " +
-                   "WHERE oid=?";
-	PreparedStatement preStmt = connection.preparedStatement(stmt);
-	preStmt.setString(1,oid);
-	try {
-		ResultSet rs = preStmt.executeUpdate(sql);
-		return re.getInt("num");
-	}
-	catch (SQLException e) {return -1;}
-  
-  public int getCountriesNextToOceanCount(int oid) {
-	return -1;
-  }
- 
-  public String getOceanInfo(int oid){
-   return "";
-  }
+    // A connection to the database  
+    Connection connection;
 
-  public boolean chgHDI(int cid, int year, float newHDI){
-   return false;
-  }
+    // Statement to run queries
+    Statement sql;
 
-  public boolean deleteNeighbour(int c1id, int c2id){
-   return false;        
-  }
-  
-  public String listCountryLanguages(int cid){
-	return "";
-  }
-  
-  public boolean updateHeight(int cid, int decrH){
-    return false;
-  }
-    
-  public boolean updateDB(){
-	return false;    
-  }
-  
+    // Prepared Statement
+    PreparedStatement ps;
+
+    // Resultset for the query
+    ResultSet rs;
+
+    //CONSTRUCTOR
+    Assignment2() {
+        try {	//Load JDBC driver
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            return;
+        }
+    }
+
+    //Using the input parameters, establish a connection to be used for this session. Returns true if connection is sucessful
+    public boolean connectDB(String URL, String username, String password) {
+        try {
+            connection = DriverManager.getConnection(URL, username, password);
+            if (connection == null) {
+                return false;
+            }
+            sql = connection.createStatement();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    //Closes the connection. Returns true if closure was sucessful
+    public boolean disconnectDB() {
+        try {
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertCountry(int cid, String name, int height, int population) {
+        String stmt = "INSERT INTO country "
+                + "VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement preStmt = connection.prepareStatement(stmt);
+            preStmt.setString(1, Integer.toString(cid));
+            preStmt.setString(2, name);
+            preStmt.setString(3, Integer.toString(height));
+            preStmt.setString(4, Integer.toString(population));
+            preStmt.executeUpdate();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getCountriesNextToOceanCount(int oid) {
+        String stmt = "SELECT COUNT(cid) AS num FROM oceanAccess "
+                + "WHERE oid=?";
+        try {
+            PreparedStatement preStmt = connection.prepareStatement(stmt);
+            preStmt.setString(1, Integer.toString(oid));
+            ResultSet resSet = preStmt.executeQuery();
+            return resSet.getInt("num");
+        } catch (SQLException e) {
+        }
+        return -1;
+    }
+
+    public String getOceanInfo(int oid) {
+        return "";
+    }
+
+    public boolean chgHDI(int cid, int year, float newHDI) {
+        return false;
+    }
+
+    public boolean deleteNeighbour(int c1id, int c2id) {
+        return false;
+    }
+
+    public String listCountryLanguages(int cid) {
+        return "";
+    }
+
+    public boolean updateHeight(int cid, int decrH) {
+        return false;
+    }
+
+    public boolean updateDB() {
+        return false;
+    }
+
 }
